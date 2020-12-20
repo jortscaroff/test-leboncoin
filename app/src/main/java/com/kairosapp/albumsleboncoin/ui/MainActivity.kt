2bd52514f22884a3d4650befad483299f4af471c
jortscaroff <jortscaroff@gmail.com>
2020-12-20 16:55:07 +0100

    image placeholder & loading view

--- app/src/main/java/com/kairosapp/albumsleboncoin/ui/MainActivity.kt
+++ app/src/main/java/com/kairosapp/albumsleboncoin/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kairosapp.albumsleboncoin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,6 +11,7 @@ import com.kairosapp.albumsleboncoin.R
 import com.kairosapp.albumsleboncoin.ui.recycler.AlbumAdapter
 import com.kairosapp.albumsleboncoin.ui.viewmodel.AlbumListViewModelImpl
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,18 +23,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        album_recyclerView.layoutManager = LinearLayoutManager(this)
+        album_recycler_view.layoutManager = LinearLayoutManager(this)
 
         albumListViewModel.state.observe(this, Observer { state ->
             Log.d("MainActivity", "onCreate: $state")
             when (state) {
                 AlbumListViewModelImpl.State.Loading -> {
+                    album_recycler_view.visibility = View.GONE
+                    album_progress_bar.visibility = View.VISIBLE
                 }
                 is AlbumListViewModelImpl.State.Loaded -> {
-                    Log.d("MainActivity", "onCreate: Loaded")
-                    album_recyclerView.adapter = AlbumAdapter(state.albumList)
+                    album_progress_bar.visibility = View.GONE
+                    album_recycler_view.visibility = View.VISIBLE
+
+                    album_recycler_view.adapter = AlbumAdapter(this, state.albumList)
                 }
                 is AlbumListViewModelImpl.State.Error -> {
+                    album_progress_bar.visibility = View.GONE
                 }
             }
         })
--- app/src/main/java/com/kairosapp/albumsleboncoin/ui/recycler/AlbumAdapter.kt
+++ app/src/main/java/com/kairosapp/albumsleboncoin/ui/recycler/AlbumAdapter.kt
@@ -1,15 +1,36 @@
 package com.kairosapp.albumsleboncoin.ui.recycler
 
+import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.kairosapp.albumsleboncoin.R
 import com.kairosapp.albumsleboncoin.model.Album
+import com.squareup.picasso.OkHttp3Downloader
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.album_row.view.*
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import java.io.File
 
-class AlbumAdapter(private val albumList: List<Album>): RecyclerView.Adapter<AlbumViewHolder>() {
+private const val PICASSO_DISK_CACHE_SIZE_LIMIT = 25L * 1024L * 1024L // 25mo
+
+class AlbumAdapter(context: Context, private val albumList: List<Album>): RecyclerView.Adapter<AlbumViewHolder>() {
+
+    val picasso = Picasso.Builder(context)
+        .downloader(
+            OkHttp3Downloader(
+                OkHttpClient.Builder()
+                    .cache(Cache(File(context.cacheDir, "images"), PICASSO_DISK_CACHE_SIZE_LIMIT))
+                    .build()
+            )
+        )
+        .build()
+
+
     override fun getItemCount(): Int {
         return albumList.size
     }
@@ -24,7 +45,11 @@ class AlbumAdapter(private val albumList: List<Album>): RecyclerView.Adapter<Alb
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
         val album = albumList[position]
         holder.itemView.album_title.text = album.title
-        Picasso.get().load(album.thumbnailUrl).into(holder.itemView.album_thumbnail);
+
+        val placeholder = ColorDrawable(ContextCompat.getColor(holder.itemView.context, R.color.image_placeholder_background))
+
+        picasso.load(album.thumbnailUrl).placeholder(placeholder).into(holder.itemView.album_thumbnail);
+
     }
 }
 
